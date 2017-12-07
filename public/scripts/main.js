049b0d4a2cabea93d792a44731029d5da42442d1
Katie D'Angelo <kd.dangelo@gmail.com>
2017-12-07 16:30:01 -0500

    added wowjs and port section

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,42 +7,28 @@ $(function () {
 
     setTimeout(function () {
         $(".header--typed").typed({
-            strings: ["a Web Developer.", "^222 an Avid Reader.", "^111 A Documentary Lover", "^111 Blah Blah", "^111 More Blah Blah"], // ^{number} denotes time in ms to wait
-
-            // typing speed
+            strings: ["a Web Developer.", "^222 an Avid Reader.", "^111 A Documentary Lover", "^111 Blah Blah", "^111 More Blah Blah"],
             typeSpeed: 90,
-
-            // time before typing starts
             startDelay: 0,
-
-            // backspacing speed
             backSpeed: 65,
-
-            // time before backspacing
             backDelay: 900,
-
-            // loop
             loop: true,
-
-            // false = infinite
             loopCount: false,
-
-            // show cursor
             showCursor: true,
-
-            // character for cursor
             cursorChar: "",
-
-            // attribute to type (null == text)
             attr: null,
-
-            // either html or text
             contentType: 'html',
-
-            // call when done callback function
             callback: function callback() {}
         });
     });
+
+    new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 0, // default
+        mobile: false,
+        live: true // default
+    }).init();
 });
 
 // appTotal.events = function () {}
